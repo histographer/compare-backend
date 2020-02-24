82c32370463f4dcd1659344986b8e473f65acc3c
Jon Wallem Anundsen <j.w.anundsen@protonmail.com>
2020-02-24 11:50:13 +0100

    feat: add image DAO

---
+++ src/main/java/no/digipat/patornat/mongodb/dao/MongoImageDAO.java
@@ -0,0 +1,28 @@
+package no.digipat.patornat.mongodb.dao;
+
+import org.bson.Document;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+
+/**
+ * A data access object (DAO) for images.
+ * 
+ * @author Jon Wallem Anundsen
+ *
+ */
+public class MongoImageDAO {
+    
+    private final MongoCollection<Document> collection;
+    
+    /**
+     * Creates a DAO.
+     * 
+     * @param client the client used to connect to the database
+     * @param database the name of the database
+     */
+    public MongoImageDAO(MongoClient client, String database) {
+        this.collection = client.getDatabase(database).getCollection("Image");
+    }
+    
+}
