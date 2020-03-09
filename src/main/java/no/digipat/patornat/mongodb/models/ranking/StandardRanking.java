558c83d398dc933761ec984098f6f032aa00c40b
kentare <kentat@stud.ntnu.no>
2020-03-09 14:55:19 +0100

    feat: add StandardRanking class

---
+++ src/main/java/no/digipat/patornat/mongodb/models/ranking/StandardRanking.java
@@ -0,0 +1,34 @@
+package no.digipat.patornat.mongodb.models.ranking;
+
+public class StandardRanking {
+    private Long id;
+    private float score;
+    private int numberOfRankings;
+
+    public Long getId() {
+        return id;
+    }
+
+    public StandardRanking setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public float getScore() {
+        return score;
+    }
+
+    public StandardRanking setScore(float score) {
+        this.score = score;
+        return this;
+    }
+
+    public int getNumberOfRankings() {
+        return numberOfRankings;
+    }
+
+    public StandardRanking setNumberOfRankings(int numberOfRankings) {
+        this.numberOfRankings = numberOfRankings;
+        return this;
+    }
+}
