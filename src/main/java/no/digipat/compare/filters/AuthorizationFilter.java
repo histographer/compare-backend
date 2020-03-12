5d7f0adce389271c462218febbbd4ee7fd7dff4a
Jon Wallem Anundsen <j.w.anundsen@protonmail.com>
2020-03-12 14:52:43 +0100

    feat: add authentication check to /imagePair

--- src/main/java/no/digipat/compare/filters/AuthorizationFilter.java
+++ src/main/java/no/digipat/compare/filters/AuthorizationFilter.java
@@ -11,7 +11,10 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebFilter("/scoring")
+@WebFilter(urlPatterns = {
+        "/scoring",
+        "/imagePair"
+})
 public class AuthorizationFilter implements Filter {
     private FilterConfig config;
 
