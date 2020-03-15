46ce3df0f6bc8377270d1c81ec3b485f72686338
kentare <kentat@stud.ntnu.no>
2020-03-15 12:52:34 +0100

    feat: add projectId as queryParameter string

--- src/main/java/no/digipat/compare/servlets/NextImagePairServlet.java
+++ src/main/java/no/digipat/compare/servlets/NextImagePairServlet.java
@@ -77,7 +77,8 @@ public class NextImagePairServlet extends HttpServlet {
         MongoClient client = (MongoClient) context.getAttribute("MONGO_CLIENT");
         String databaseName = (String) context.getAttribute("MONGO_DATABASE");
         MongoImageDAO imageDao = new MongoImageDAO(client, databaseName);
-        List<Image> images = imageDao.getAllImages(1L); // TODO project ID
+        Long projectId = Long.parseLong(request.getParameter("projectId"));
+        List<Image> images = imageDao.getAllImages(projectId); // TODO project ID
         // The project ID is currently hard coded to allow compilation
         if (images.size() < 2) {
             throw new ServletException("Not enough images in the database");
