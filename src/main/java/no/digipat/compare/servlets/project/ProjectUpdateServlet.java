31361b060a7c77c8a5479ac85a94efab49359eb1
Jon Wallem Anundsen <j.w.anundsen@protonmail.com>
2020-03-20 01:37:32 +0100

    refactor: update ProjectUpdateServlet

--- src/main/java/no/digipat/compare/servlets/project/ProjectUpdateServlet.java
+++ src/main/java/no/digipat/compare/servlets/project/ProjectUpdateServlet.java
@@ -1,16 +1,12 @@
 package no.digipat.compare.servlets.project;
 
-import be.cytomine.client.CytomineConnection;
-import be.cytomine.client.CytomineException;
 import com.mongodb.MongoClient;
-import no.digipat.compare.models.image.Image;
 import no.digipat.compare.models.project.Project;
-import no.digipat.compare.mongodb.dao.MongoImageDAO;
 import no.digipat.compare.mongodb.dao.MongoProjectDAO;
-import org.json.JSONArray;
+
+import org.json.JSONException;
 import org.json.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+import org.json.JSONTokener;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -18,9 +14,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.List;
 
 @WebServlet(name = "no.digipat.compare.servlets.project.ProjectUpdateServlet", urlPatterns = {"/project/update"})
 public class ProjectUpdateServlet extends HttpServlet {
@@ -28,46 +22,53 @@ public class ProjectUpdateServlet extends HttpServlet {
 
     /**
      *
-     * Updates a project active status
-     *  needs query parameters {@code projectId} which is long and {@active} which is bool
-     *
+     * Updates a project's active status. The request body must contain
+     * a JSON object of the following form:
+     * 
+     * <pre>
+     *   {
+     *     "projectId": &lt;long&gt;,
+     *     "active": &lt;boolean&gt;
+     *   }
+     * </pre>
+     * 
+     * The response body will contain a representation of the project's
+     * state after the update, represented by a JSON object of the following form:
+     * 
+     * <pre>
+     *   {
+     *     "id": &lt;long&gt;,
+     *     "active": &lt;boolean&gt;,
+     *     "name": &lt;String&gt;
+     *   }
+     * </pre>
+     * 
      * @param request the request
      * @param response the response
      *
      * @throws IOException if an I/O error occurs
      */
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         ServletContext context = getServletContext();
-        Long projectId = null;
-        Boolean active = null;
         MongoClient client = (MongoClient) context.getAttribute("MONGO_CLIENT");
         String databaseName = (String) context.getAttribute("MONGO_DATABASE");
         MongoProjectDAO projectDao = new MongoProjectDAO(client, databaseName);
-        response.setContentType("application/json");
         try {
-            projectId = Long.parseLong(request.getParameter("projectId"));
-            active = Boolean.parseBoolean(request.getParameter("active"));
-            if(!projectDao.projectExists(projectId)) {
-                response.setStatus(HttpServletResponse.SC_NO_CONTENT);
-                response.getWriter().print("Project does not exist in database");
-            }
-            {
+            JSONObject json = new JSONObject(new JSONTokener(request.getInputStream()));
+            long projectId = json.getLong("projectId");
+            boolean active = json.getBoolean("active");
+            if (!projectDao.projectExists(projectId)) {
+                response.sendError(HttpServletResponse.SC_NOT_FOUND);
+            } else {
                 Project project = projectDao.updateProjectActive(projectId, active);
+                response.setContentType("application/json");
                 response.getWriter().print(getProjectResponse(project));
-
             }
-        } catch(NumberFormatException e) {
-            response.getWriter().print(e);
-        } catch (Exception e) {
-            response.getWriter().print(e);
+        } catch(JSONException e) {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST);
         }
-
-        if(projectId == null || active == null) {
-            response.getWriter().print("One of the parameters is not set: projectId="+projectId+". active: "+active+".");
-        }
-
     }
 
     private static JSONObject getProjectResponse(Project project) {
